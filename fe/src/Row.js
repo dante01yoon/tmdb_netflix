51c681bf0def1532c02c3d0d48fbcbeac860960a
risel <bunggl@naver.com>
2020-08-06 23:29:49 +0900

    Update: homepage markup done

--- fe/src/Row.js
+++ fe/src/Row.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import instance from "./axios";
+
 import "./Row.css";
+
 const ImageSource = "https://image.tmdb.org/t/p/original";
 
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   const [movies, setMovies] = useState([]);
+  const [ trailerUrl, setTrailerUrl] = useState("");
   useEffect(() => {
     new Promise((resolve) => {
       resolve(instance.request({ method: "GET", url: fetchUrl }));
@@ -12,22 +17,43 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
       setMovies(value.data.results);
     });
   }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay:1,
+    }
+  }
+  const handleClick = (movie) => {
+    console.log(movie);
+    if(trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+      .then(url => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get('v'));
+      }).catch(error => console.log(error)); 
+    }
+  }
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {/* 
-      sevral row posters */}
+        
         {movies.map((movie) => (
           movie.poster_path && movie.backdrop_path && 
           <img 
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className="row__poster"
             src={`${ImageSource}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
             alt={movie.name} 
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} /> } 
     </div>
   );
 };
